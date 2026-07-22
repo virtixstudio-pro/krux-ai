00cc7e50dcf9529cc9ad065f17bb486ca11eac98
virtixstudio-pro <virtixstudio@gmail.com>
2026-07-22 19:36:51 +0200

    Add missing Java activities and layout XML files

---
+++ app/src/main/java/com/studio/krux/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.studio.krux;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 1500);
+    }
+}
